30a9f0cf7e8381abb396d6606c6903da0c8f1cdc
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-03-06 14:27:02 -0600

    can send data to Firebase

--- postFirebase.js
+++ postFirebase.js
@@ -2,30 +2,37 @@ $(document).ready(function(){
 	var firebaseLink = 'https://campusspace.firebaseio.com/';
 	var rectangles = new Firebase(firebaseLink);
 	$("#SubmitRectangle").on('click', function(){
-		console.log($("#semesters").val());
-		// console.log($("#"))
+		var semestersNum = $("#semesters").val();
+		var collegeNames = [];
+		var vehicleNames = [];
 
+		$('#colleges:checked').each(function() {
+		    collegeNames.push($(this).val());
+		});
 
-// 		console.log('Loading:');
-// 		var user = {
-// 			"college": ['LAS'],
-// 			"semester": 2,
-// 			"transportation": ["bike", "car"],
-// 			"house": [(1, 3), (5, 7)],
-// 		}
-// 		var rect = {
-// 			"label": "Grainger",
-// 			"coord": [(1, 1), (2, 4)]
-// 		}
+		$('#vehicles:checked').each(function() {
+		    vehicleNames.push($(this).val());
+		});
 
-// 		var rectangle = {
-// 			"user_info": user,
-// 			"rectangle": rect
-// 		}
+		console.log('Loading:');
+		var user = {
+			"college": collegeNames,
+			"semester": semestersNum,
+			"transportation": vehicleNames,
+			"house": [[1, 3], [5, 7]],
+		}
+		var rect = {
+			"label": "Grainger",
+			"coord": [[1, 1], [2, 4]]
+		}
 
-// 		rectangles.push(rectangle);
-// 		console.log('Posted:');
-// 	});
-// });
+		var rectangle = {
+			"user_info": user,
+			"rectangle": rect
+		}
 
-	
+		rectangles.push(rectangle);
+		console.log('Posted:');
+ 	});
+ });
+
